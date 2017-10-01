585b79e99c93f3cfa1323f59febe2cb7e81a00f8
Audrius Naujokas <audrius.naujokas@gmail.com>
2017-10-01 22:40:45 +0300

    Sukurtas xml failas ir readFile klase

---
+++ Fill Up!/Fill Up!/Fill_Up_/ReadFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Fill_Up_.Fill_Up_
+{
+    class ReadFile
+    {
+        
+    }
+}
