78959481fc371957b1f810321e472d0c53d00896
SaruneUrvikyte <sarune.urvikyte@gmail.com>
2017-10-01 11:39:30 +0300

    UI papildymas, klasės Bar sukūrimas

---
+++ Fill Up!/Fill Up!/Fill_Up_/Bar.cs
@@ -0,0 +1,20 @@
+﻿namespace Fill_Up_
+{
+    internal class Bar
+    {
+        private string name;
+        private int rating;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Rating
+        {
+            get { return rating; }
+            set { rating = value; }
+        }
+    }
+}
--- Fill Up!/Fill Up!/Properties/Resources.Designer.cs
+++ Fill Up!/Fill Up!/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace Fill_Up_.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Favorites_icon {
-            get {
-                object obj = ResourceManager.GetObject("Favorites-icon", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
