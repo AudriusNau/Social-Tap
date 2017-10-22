5ac10478133d9a2a152f77f96a4e325c2f4bf709
SaruneUrvikyte <sarune.urvikyte@gmail.com>
2017-10-22 16:04:24 +0300

    Sutvarkytos klaidos klasėje Rating.cs , metami pranešimai, jei palikti tušti laukai.

--- Fill Up!/Fill Up!/Rating.cs
+++ Fill Up!/Fill Up!/Rating.cs
@@ -16,54 +16,43 @@ namespace Fill_Up_
 
         private void button1_Click(object sender, EventArgs e) //issaugoti
         {
+            if (textBarName.Text == string.Empty)
+            {
+                MessageBox.Show(Rating1.Error1);
+                return;
+            }
+
+            if (domainUpDownRating.Text == string.Empty)
+            {
+                MessageBox.Show(Rating1.Error2);
+                return;
+            }
+
             GlassOfBeer glass = new GlassOfBeer();
 
             glass.lackOfBeer = (int)numericUpDown1.Value;
             glass.price = numericUpDown2.Value;
-                                                //reiktu pakeisti i switch
+
             if (radioButton1.Checked == true)
             {
-                glass.orderedQuantity = Rating1.Value1;
+                glass.orderedQuantity = double.Parse(Rating1.Value1);
             }
             else if (radioButton2.Checked == true)
             {
-                glass.orderedQuantity = Rating1.Value2;
+                glass.orderedQuantity = double.Parse(Rating1.Value2);
             }
             else if (radioButton3.Checked == true)
             {
-                glass.orderedQuantity = Rating1.Value3;
+                glass.orderedQuantity = double.Parse(Rating1.Value3);
             }
-            else
-                glass.orderedQuantity = Rating1.Value4;
-
+            else 
+                glass.orderedQuantity = double.Parse(Rating1.Value4);
+            
             VisitedBar bar = new VisitedBar(textBarName.Text, int.Parse(domainUpDownRating.Text), glass);
             allBars.AddNewBar(bar);
 
-            List<VisitedBar> x = allBars.GetBarList();
-            MessageBox.Show(x.Count.ToString());
-
             this.Hide();
             new Results(allBars, bar).Show();
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBarName_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label6_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Rating_Load(object sender, EventArgs e)
-        {
-
         }
     }
 }
