bea729c7826faad5f6cd7070564ded6cc102ded9
Rūta <ru.stankeviciute@gmail.com>
2017-10-25 11:45:50 +0300

    Žemėlapių interfeisas.

---
+++ Fill Up!/Fill Up!/Fill_Up_/GoogleMaps.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fill_Up_.Fill_Up_
+{
+    public class GoogleMaps : IMap
+    {
+        public string Name { get { return "GoogleMaps"; } }
+        public string Url { get { return FindInMap1.GoogleMaps; } }
+
+        public GoogleMaps()
+        {
+
+        }
+    }
+}
---
+++ Fill Up!/Fill Up!/Fill_Up_/MapFactory.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Fill_Up_.Fill_Up_
+{
+    class MapFactory
+    {
+        private List<IMap> mapList;
+
+        public MapFactory()
+        {
+            this.mapList = new List<IMap>();
+        }
+
+        public void AddNewMap(IMap map)
+        {
+            this.mapList.Add(map);
+        }
+
+        public List<IMap> GetMapList()
+        {
+            return mapList;
+        }
+
+        public string GetUrl(string name)
+        {
+            foreach(IMap m in mapList)
+            {
+                if (m.Name == name)
+                    return m.Url;
+            }
+            return null;
+        }
+    }
+}
