c33221aedf26d6d08bc43662510d4469948a1822
Šarūnė <sarune.urvikyte@gmail.com>
2017-10-25 20:30:56 +0300

    šarūnės branch (#37)
    * Sutvarkytos klaidos klasėje Rating.cs , metami pranešimai, jei palikti tušti laukai.
    
    * Ištrinti nereikalingi komentarai.
    
    * Minimalūs pataisymai
    
    * Sutvarkyti .resx failai, sukurti LINQ sakiniai
    
    * Paprasti metodai ištrinti, palikti LINQ sakiniai
    
    * Kodo pataisymai
    
    * pakeitimai
    
    * pakeitimai :)))))))))))))))))
    
    * Pakeitimai atsiskaitymo metu, Extension.cs klasės sukūrimas

---
+++ Fill Up!/Fill Up!/Fill_Up_/Extension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fill_Up_.Fill_Up_
+{
+    public static class Extension
+    {
+        public static double StringToDouble(this string value)
+        {
+            return Double.Parse(value);
+        }
+    }
+}
