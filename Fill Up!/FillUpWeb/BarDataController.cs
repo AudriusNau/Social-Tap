a18f14439f2e21e84fe9620b27511151e5531326
Rūta <ru.stankeviciute@gmail.com>
2017-11-12 22:17:03 +0200

    Web Service pradžia.

---
+++ Fill Up!/FillUpWeb/BarDataController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace FillUpWeb
+{
+    public class BarDataController : ApiController
+    {
+        BarData[] bars = new BarData[]
+        {
+            new BarData { Name = "Snekutis", Rating = 5 },
+            new BarData { Name = "Skybar", Rating = 4 }
+        };
+
+        public IEnumerable<BarData> GetAllBars()
+        {
+            return bars;
+        }
+
+        public BarData GetBarByRating(int rating)
+        {
+            var bar = bars.FirstOrDefault((p) => p.Rating == rating);
+            if (bar == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return bar;
+        }
+
+    }
+}
