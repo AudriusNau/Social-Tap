e9abe8211c97de27b162fa00eea05d7c2843b244
Audrius Naujokas <audrius.naujokas@gmail.com>
2017-11-13 20:00:21 +0200

    a

--- Fill Up!/Fill Up App/Resources/Resource.Designer.cs
+++ Fill Up!/Fill Up App/Resources/Resource.Designer.cs
@@ -60,8 +60,11 @@ namespace Fill_Up_App
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050001
+			public const int button1 = 2131034113;
+			
 			// aapt resource value: 0x7f050000
-			public const int button1 = 2131034112;
+			public const int button2 = 2131034112;
 			
 			static Id()
 			{
