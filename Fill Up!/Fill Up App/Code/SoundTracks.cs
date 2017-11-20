a75545e23c0d06dc80804eaed8bae712b2785367
Audrius Naujokas <audrius.naujokas@gmail.com>
2017-11-20 12:39:26 +0200

    Soundtrack added

--- Fill Up!/Fill Up App/Code/SoundTracks.cs
+++ Fill Up!/Fill Up App/Code/SoundTracks.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
 using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.Media;
 
 namespace Fill_Up_App.Code
 {
     class SoundTracks
     {
+        public static void SoundTrack()
+        {
+            var play = new MediaPlayer();
+            MediaPlayer player = new MediaPlayer();
+            string path = @"Fly_Me_To_The_Moon.wav";
+            player.SetDataSource(path);
+            player.Start();
+
+        }
     }
 }
