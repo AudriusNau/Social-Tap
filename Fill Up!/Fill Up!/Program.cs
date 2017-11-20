82fae828390d9b3d16e05d414f6c50e8bd73c3e7
Audrius Naujokas <audrius.naujokas@gmail.com>
2017-11-20 11:48:31 +0200

    SoundTrack added

--- Fill Up!/Fill Up!/Program.cs
+++ Fill Up!/Fill Up!/Program.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Windows.Forms;
 using Fill_Up_.Fill_Up_;
+using System.Threading;
 
 namespace Fill_Up_
 {
     static class Program
     {
+        public static void SoundTrack()
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+            string path = @"Fly_Me_To_The_Moon.wav";
+            player.SoundLocation = path;
+            player.Play();
+            
+        }
         [STAThread]
         static void Main()
         {
             ListsOfBars allbars = new ListsOfBars();
-            
+
+            Thread th = new Thread(SoundTrack);
+            th.Start();
+
             ReadFile load = new ReadFile();
             load.ReadData(allbars);
            
